53aa3e5b614dd5ee9c2a186f216b848409d13400
WilmerAraya <wilmeraraya21@gmail.com>
2024-03-11 23:12:44 -0600

    Adding pause feature on song cards

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,6 +1,4 @@
 import { FaPlayCircle, FaPauseCircle } from 'react-icons/fa';
-import { usePlayer } from '../hooks/usePlayer';
-import { useGetGlobalTopSongsQuery } from '../redux/services/spotifyAPI';
 
 const PlayPause = ({
   song,
@@ -9,12 +7,14 @@ const PlayPause = ({
   handlePlayClick,
   handlePauseClick,
 }) => {
-  // console.log(isPlaying, activeSong, song);
-
   return (
     <div className="flex justify-center items-center h-full w-full">
       {isPlaying && activeSong?.id === song.id ? (
-        <FaPauseCircle size={35} />
+        <FaPauseCircle
+          className="animate-fastfade text-gray-300"
+          size={45}
+          onClick={handlePauseClick}
+        />
       ) : (
         <FaPlayCircle
           className="animate-fastfade text-gray-300"
