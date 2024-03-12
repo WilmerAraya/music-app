1913906e077f3aba4dd42764297640004ca1af90
WilmerAraya <wilmeraraya21@gmail.com>
2024-03-11 18:34:01 -0600

    adding card song component

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,5 +1,47 @@
-const SongCard = () => (
-  <div>SongCard</div>
-);
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
+import PlayPause from './PlayPause';
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
+
+const SongCard = ({ song, isPlaying, activeSong, index }) => {
+  const handlePauseClick = () => {};
+
+  const handlePlayClick = () => {};
+
+  return (
+    <div
+      className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80
+     backdrop-blur-sm animate-slideup rounded-lg cursor-pointer"
+    >
+      <div className="relative w-full h-56 group">
+        <div
+          className={`absolute inset-0 justify-center items-center
+         bg-black bg-opacity-50 group-hover:flex ${
+           activeSong?.title === song ? 'flex bg-black bg-opacity-70' : 'hidden'
+         }`}
+        >
+          <PlayPause
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            song={song}
+            handlePause={handlePauseClick}
+            handlePlayClick={handlePlayClick}
+          />
+        </div>
+        <img alt="song_img" src={song.album.images[0]?.url} />
+      </div>
+      <div className="mt-4 flex flex-col">
+        <p className="font-semibold text-lg text-primary capitalize truncate">
+          <Link to={`/songs/${song?.id}`}>{song.name}</Link>
+        </p>
+        <p className="text-sm truncate text-gray-300 mt-1">
+          <Link to={song.artists ? song.album.artists[0].id : '/'}>
+            {song.album.artists[0].name}
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
 export default SongCard;
