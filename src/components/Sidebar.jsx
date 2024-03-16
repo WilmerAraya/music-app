ed6d077f044b15fab04994120a2d2d15f6630f51
WilmerAraya <wilmeraraya21@gmail.com>
2024-03-16 03:39:13 -0600

    Making sidebar collapsable

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,22 +2,29 @@ import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { RiCloseLine } from 'react-icons/ri';
 import { HiOutlineMenu } from 'react-icons/hi';
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
 
-import { logoWithName } from '../assets';
+import { logoWithName, logo } from '../assets';
 import { links } from '../assets/constants';
 
-const NavLinks = ({ handleClick }) => (
+const NavLinks = ({ handleClick, collapsed }) => (
   <div className="mt-10">
     {links.map((link) => (
       <NavLink
         key={link.name}
         to={link.to}
-        className="flex flex-row justify-start items-center my-8 text-sm
+        className="flex flex-row shrink-0 grow-0 justify-start items-center my-8 text-sm
         font-medium text-gray-400"
         onClick={() => handleClick && handleClick()}
       >
-        <link.icon className="w-6 h-6 mr-2" />
-        {link.name}
+        <link.icon className="w-10 h-10 mr-2 flex flex-shrink-0" />
+        <span
+          className={`${
+            collapsed && 'hidden'
+          } whitespace-nowrap origin-left duration-200`}
+        >
+          {link.name}
+        </span>
       </NavLink>
     ))}
   </div>
@@ -25,16 +32,41 @@ const NavLinks = ({ handleClick }) => (
 
 const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [collapsed, setCollapsed] = useState(false);
 
   return (
     <>
-      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-sidebar">
+      <div
+        className={`relative duration-300 md:flex hidden flex-col ${
+          collapsed ? 'w-20' : 'w-60'
+        } pb-10 pt-5 px-4 bg-sidebar`}
+      >
+        <div
+          className="absolute text-primary top-20 -right-3
+          cursor-pointer hover:scale-125 duration-200 hover:text-active"
+          onClick={() => setCollapsed(!collapsed)}
+        >
+          {collapsed ? (
+            <FaArrowAltCircleRight className="w-8 h-8" />
+          ) : (
+            <FaArrowAltCircleLeft className="w-8 h-8" />
+          )}
+        </div>
         <img
-          src={logoWithName}
+          src={logo}
           alt="logo"
-          className="w-40 h-20 self-center object-contain"
+          className=" w-10 h-10 self-center object-contain"
         />
-        <NavLinks />
+        <div className="w-full text-center">
+          <p
+            className={`${
+              collapsed && 'scale-0'
+            } duration-300 origin-left text-2xl text-primary font-bold`}
+          >
+            SongSpot
+          </p>
+        </div>
+        <NavLinks collapsed={collapsed} />
       </div>
 
       <div className="absolute md:hidden block top-6 right-3">
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -13,8 +13,8 @@ const Discover = () => {
 
   return (
     <div className="flex flex-col">
-      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
-        <h2 className="font-bold text-3xl text-primary text-left">
+      <div className=" flex justify-center items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold text-3xl text-primary text-left sm:mr-10 ">
           Discover {genreTitle}
         </h2>
         <select
@@ -29,7 +29,7 @@ const Discover = () => {
           ))}
         </select>
       </div>
-      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+      <div className="flex flex-wrap sm:justify-center justify-center gap-8">
         {data.items?.map((item, index) => (
           <SongCard
             key={item.track.id}
